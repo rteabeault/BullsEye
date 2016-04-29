d4decd4182584db788b0d61d712a769e38133dbd
Russell Teabeault <rteabeault@gmail.com>
2016-04-29 15:17:53 -0600

    Finishing work

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class AboutViewController: UIViewController {
-
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+            if let htmlData = NSData(contentsOfFile: htmlFile) {
+                let baseUrl = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath)
+                webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseUrl)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
